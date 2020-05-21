dfdaea479e75713820cc3f06eec667b3b24ec99f
whitequartz <nadim.abdulgani@mail.ru>
2020-05-21 20:44:34 +0300

    database.db

--- database.go
+++ database.go
@@ -10,11 +10,30 @@ type user struct {
 	email    string
 	password string
 	dname    string
+	tp       int
+}
+
+type issue struct {
+	issue_id    int
+	status      int
+	dname       string
+	content     string
+	user_id     int
+	tp_id       int
+	data_create int
+}
+
+type message struct {
+	msg_id      int
+	issue_id    int
+	dtype       string
+	content     string
+	data_create int
 }
 
 // создаёт запись в дб по заданным параметрам
-func register(db *sql.DB, email string, password string, name string) int64 {
-	result, err := db.Exec("INSERT INTO users (email,password,dname) VALUES ($1,$2,$3)", email, password, name)
+func register(db *sql.DB, email string, password string, name string, tp int) int64 {
+	result, err := db.Exec("INSERT INTO users (email,password,dname,tp) VALUES ($1,$2,$3,$4)", email, password, name, tp)
 	if err != nil {
 		panic(err)
 	}
@@ -31,7 +50,7 @@ func getUserId(db *sql.DB, email string) int {
 	defer result.Close()
 	if result.Next() {
 		user := user{}
-		err = result.Scan(&user.id, &user.email, &user.password, &user.dname)
+		err = result.Scan(&user.id, &user.email, &user.password, &user.dname, &user.tp)
 		if err != nil {
 			panic(err)
 		}
@@ -61,20 +80,24 @@ func checkPassword(db *sql.DB, email string, password string) string {
 	return ""
 }
 
+func getIssues(db *sql.DB) {
+
+}
+
 func database() *sql.DB {
 	db, err := sql.Open("sqlite3", "database.db")
 	if err != nil {
 		panic(err)
 	}
-	_, err = db.Exec("CREATE TABLE if not exists users (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, email text, password text, dname text)")
+	_, err = db.Exec("CREATE TABLE if not exists users (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, email text, password text, dname text, tp INTEGER)")
 	if err != nil {
 		panic(err)
 	}
-	_, err = db.Exec("CREATE TABLE if not exists issues (issue_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, status text, dname text, content text, user_id INTEGER, tp_id INTEGER, data_create INTEGER)")
+	_, err = db.Exec("CREATE TABLE if not exists issues (issue_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, status INTEGER, dname text, content text, user_id INTEGER, tp_id INTEGER, data_create INTEGER)")
 	if err != nil {
 		panic(err)
 	}
-	_, err = db.Exec("CREATE TABLE if not exists messages (msg_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, issue_id INTEGER, type text, content text, data_create INTEGER)")
+	_, err = db.Exec("CREATE TABLE if not exists messages (msg_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, issue_id INTEGER, dtype text, content text, data_create INTEGER)")
 	if err != nil {
 		panic(err)
 	}
